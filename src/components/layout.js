b2c474da9bb5cf8e4c615ab8eac9bd020f61faac
Nenad Brcic <neno@neno.ch>
2020-03-14 19:58:56 +0100

    Initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { IdentityContextProvider } from 'react-netlify-identity-widget';
+
+import './layout.css';
+
+const Layout = ({ children }) => (
+  <IdentityContextProvider url="https://jamstack-intro-auth.netflify.com">
+    <header>
+      <Link to="/">JAMstack APP</Link>
+    </header>
+    <main>{children}</main>
+  </IdentityContextProvider>
+)
+
+export default Layout;
---
+++ src/components/route-login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const RouteLogin = () => (
+  <>
+    <h1>Login or Sign up</h1>
+    <button type="button">Log In</button>
+  </>
+)
+
+export default RouteLogin;
