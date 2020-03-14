a690cb260285b1380b580171533fd3931a453860
Nenad Brcic <neno@neno.ch>
2020-03-14 20:08:18 +0100

    update identity provider url

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import { IdentityContextProvider } from 'react-netlify-identity-widget';
 import './layout.css';
 
 const Layout = ({ children }) => (
-  <IdentityContextProvider url="https://jamstack-intro-auth.netflify.com">
+  <IdentityContextProvider url="https://jamstack-intro-auth-neno.netflify.com">
     <header>
       <Link to="/">JAMstack APP</Link>
     </header>
